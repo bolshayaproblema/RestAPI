4beb4e118cb1e06d42ec1ba86de9ccda438df6d8
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-07-04 18:53:29 +0300

    added DisplayName

--- src/test/java/tests/ReqresInTests.java
+++ src/test/java/tests/ReqresInTests.java
@@ -12,6 +12,7 @@ import static org.hamcrest.Matchers.is;
 public class ReqresInTests extends TestBase {
 
     @Test
+    @DisplayName("authorization test")
     void successFulLoginTest() {
         String requestBody = "{\n" +
                 "    \"email\": \"eve.holt@reqres.in\",\n" +
@@ -24,80 +25,88 @@ public class ReqresInTests extends TestBase {
                 .body(requestBody)
                 .when()
                 .post("/login")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(200)
-            .body("token", is("QpwL5tke4Pnpja7X4"));
-}
-@Test
-    void negativeLogin400Test(){
-    String requestBody = "{\n" +
-            "    \"email\": \"eve.holt@reqres.in\",\n" +
-            "    \"password\": \"cityslicka\"\n" +
-            "}"; // Bad Practice
-    given()
-            .log().uri()
-            .log().body()
-            .body(requestBody)
-            .when()
-            .post("/login")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(400)
-            .body("error", is ("Missing email or username"));
-}
-@Test
-    void negativeLoginTest(){
-    given()
-            .log().uri()
-            .log().body()
-            .when()
-            .post("/login")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(415);
-}
- @Test
- @DisplayName("Not found user")
-    void tests404(){
-     given()
-             .log().uri()
-             .log().body()
-             .when()
-             .get("/users/23")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(404);
-}
-@Test
- @DisplayName("No Content Success status")
-    void noContentTest(){
-    given()
-            .log().uri()
-            .log().body()
-            .when()
-            .delete("/users/2")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(204);
-}@Test
- @DisplayName("existing ids")
-    void foundUsersTest(){
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    @DisplayName("negative authorization test")
+    void negativeLogin400Test() {
+        String requestBody = "{\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"cityslicka\"\n" +
+                "}"; // Bad Practice
+        given()
+                .log().uri()
+                .log().body()
+                .body(requestBody)
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    @DisplayName("Unsupported Media Type")
+    void negativeLoginTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(415);
+    }
+
+    @Test
+    @DisplayName("Not found user")
+    void tests404() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("No Content Success status")
+    void noContentTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .delete("/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("existing ids")
+    void foundUsersTest() {
         given()
                 .log().uri()
                 .log().body()
                 .when()
                 .get("/users?page=2")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(200)
-            .body("data.id", hasItems(7, 8, 9));
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", hasItems(7, 8, 9));
 }
 }
 
