19d629303c38871d5b31665f082d18795ebce1dc
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-07-10 00:12:02 +0300

    fixed LoginUnSuccessfulModel

--- src/test/java/models/LoginUnSuccessfulModel.java
+++ src/test/java/models/LoginUnSuccessfulModel.java
@@ -1,9 +1,8 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
 public class LoginUnSuccessfulModel {
-    private String Error;
+    private String error;
 }
